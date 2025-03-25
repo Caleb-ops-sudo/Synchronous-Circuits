a4cb092ce2ac364cef6246d4574515610ca4fedb
Caleb Cook <calebcook766@u.boisestate.edu>
2025-03-25 11:43:00 -0600

    Create d_latch.v

---
+++ d_latch.v
@@ -0,0 +1,17 @@
+
+module d_latch(
+    input D,
+    input E,
+    output reg Q,
+    output NotQ
+);
+    
+    always @(E, D) begin 
+        if (E)
+            Q <= D; 
+      
+    end
+
+    assign NotQ = ~Q;
+    
+endmodule
