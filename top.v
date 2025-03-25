7884cb136c7dae6bb3e86aaa612c385a3a5502ee
Caleb Cook <calebcook766@u.boisestate.edu>
2025-03-25 11:42:07 -0600

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,31 @@
+module top(
+     input [15:0] sw,
+    input btnC,
+    output [15:0] led
+);
+    d_latch(
+        .D(sw[0]),
+        .Q(led[0]),
+        .NotQ(led[1]),
+        .E(btnC)
+    );
+    memory_system(
+        .data(sw[15:8]),
+        .addr(sw[7:6]),
+        .store(btnC),
+        .memory(led)
+
+
+
+    );
+    
+ endmodule   
+    
+    
+
+    
+    
+    
+    
+
 
